be0c24435a6193e4975f304c9d495b5b0bcf57d6
barnswills <barneywilliams90@hotmail.co.uk>
2019-07-16 17:29:14 +0100

    More and more

---
+++ src/components/RandomBarChart.vue
@@ -0,0 +1,61 @@
+
+<script>
+import { Bar } from "vue-chartjs";
+
+export default {
+  extends: Bar,
+  data: () => ({
+    chartdata: {
+      labels: [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December"
+      ],
+      datasets: [
+        {
+          label: "Data One",
+          backgroundColor: "#f87979",
+          data: [10, 20, 34, 55]
+        }
+      ]
+    },
+    options: {
+      responsive: true,
+      maintainAspectRatio: false,
+      scales: {
+        yAxes: [
+          {
+            ticks: {
+              beginAtZero: true
+            },
+            gridLines: {
+              display: true
+            }
+          }
+        ]
+      }
+    },
+
+    methods: {
+      nums: () => {
+        const x = [10, 20, 34, 55];
+
+        return x;
+      }
+    }
+  }),
+
+  mounted() {
+    this.renderChart(this.chartdata, this.options);
+  }
+};
+</script>
