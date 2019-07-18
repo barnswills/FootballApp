a56195f38871246a484c9a0e29b371c9b3fae089
barnswills <barneywilliams90@hotmail.co.uk>
2019-07-18 11:19:35 +0100

    Commit 18/07 #2

--- src/components/RandomLineChart.vue
+++ src/components/RandomLineChart.vue
@@ -1,33 +1,32 @@
 <script>
 import { Line } from "vue-chartjs";
 
-import { numbers, colors } from "../utils/randomUtils";
+import { numbers } from "../utils/randomUtils";
 
 // global constants
 const YEARS = 11;
-const mColors = colors(YEARS);
 const mNumbers = numbers(YEARS);
+const mLabelData = [
+  "2009",
+  "2010",
+  "2011",
+  "2012",
+  "2013",
+  "2014",
+  "2015",
+  "2016",
+  "2017",
+  "2018",
+  "2019"
+];
 
 export default {
   extends: Line,
   data: () => ({
     chartdata: {
-      labels: [
-        "2009",
-        "2010",
-        "2011",
-        "2012",
-        "2013",
-        "2014",
-        "2015",
-        "2016",
-        "2017",
-        "2018",
-        "2019"
-      ],
+      labels: mLabelData,
       datasets: [
         {
-          label: "Data One",
           backgroundColor: "#f87979",
           data: mNumbers
         }
